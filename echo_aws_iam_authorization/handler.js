8db7fd3d7d95aa620076ee9f7f068325eb7763f7
Jonas Cheng <jonas.cheng@gmail.com>
2016-05-23 02:53:26 -0600

    init echo_aws_iam_authorization

---
+++ echo_aws_iam_authorization/handler.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports.handler = function(event, context, cb) {
+  return cb(null, {
+    message: 'Go Serverless! Your Lambda function executed successfully!',
+    request: event
+  });
+};
