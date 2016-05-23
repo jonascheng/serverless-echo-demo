78284cacec75587b1d332ccdfce68f2493a7e43f
Jonas Cheng <jonas.cheng@gmail.com>
2016-05-23 08:40:28 -0600

    created test page and updated readme

--- test/script.js
+++ test/script.js
@@ -1,53 +1,10 @@
-# serverless-echo-demo
-A simple ECHO service to demo AWS/Cognito
-
-## Install
-
-The steps below can be taken to install the project and initialize it.
-
-Open a command line terminal and cd to the location where you will be placing the serverless-survey-forms project.
-
-Clone the project directly from Github:
-
-```git clone https://github.com/trendmicro/serverless-echo-demo.git```
-
-Enter the serverless-echo-demo folder that was just created:
-
-```cd serverless-echo-demo```
-
-Initialize the project:
-
-```serverless project init```
-
-Install npm dependency modules in serverless-echo-demo:
-
-```npm install```
-
-Deploy your functions and endpoints:
-
-```serverless dash deploy```
-
-## Test in Browser/Javascript
-
-* Login `AWS Management Console` and open `Amazon API Gateway`
-* Under `APIs\serverless-echo-demo\Stages`, click on `dev`
-* Select tab `SDK Generation`
-* Set `Platform` as `Javascript`
-* Click `Generate SDK` and download an archived file
-* Decompress the download file to the `.\test` 
-* Download [aws-sdk](https://github.com/aws/aws-sdk-js/releases) from Github
-* Decompress the download file to the `.\test`
-* Create a static script.js with the following content in the previous folder
-* Replace [ap-northeast-1] and [identity-pool-id] in `.\test\script.js`
-    
-```javascript
 function foo() {
 
   AWS.config.region = 'ap-northeast-1'; // Replace with the region you deploy
 
   var cognitoidentity = new AWS.CognitoIdentity();
   var params = {
-    IdentityPoolId: 'identity-pool-id'
+    IdentityPoolId: 'ap-northeast-1:247a8715-a43d-4af7-8030-2574188bd632'
   };
 
   // Generate a Cognito ID for the 1st time, so IdentityId could be kept for future use
@@ -73,13 +30,28 @@ function foo() {
 
       var params = {
         //This is where any header, path, or querystring request params go. The key is the parameter named as defined in the API
+        echo: "echo-in-path",
         foo: "foo"
       };
+      var body = {
+        //This is where you define the body of the request
+        payload: "this is payload"
+      };
 
-      apigClient.demoGet(params).then(function(result) {
+      apigClient.echoAwsIamAuthorizationEchoGet(params, body).then(function(result) {
         //This is where you would put a success callback
         console.log(result);
-        alert("Hello foo!");
+        alert(JSON.stringify(result.data));
+      }).catch(function(result) {
+        //This is where you would put an error callback
+        console.log(result);
+        alert("Oops foo!");
+      });
+
+      apigClient.echoAwsIamAuthorizationPost(params, body).then(function(result) {
+        //This is where you would put a success callback
+        console.log(result);
+        alert(JSON.stringify(result.data));
       }).catch(function(result) {
         //This is where you would put an error callback
         console.log(result);
@@ -91,4 +63,3 @@ function foo() {
 }
 
 foo();
-```
